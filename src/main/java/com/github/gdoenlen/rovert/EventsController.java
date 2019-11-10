8fbca7c5038f69b2d021ee9554247c85fa08ec3d
George Doenlen <gdoenlen@gmail.com>
2019-11-10 10:43:31 -0500

    use inner type if type is event_callback

--- src/main/java/com/github/gdoenlen/rovert/EventsController.java
+++ src/main/java/com/github/gdoenlen/rovert/EventsController.java
@@ -23,9 +23,24 @@ public class EventsController {
         this.handlers = handlers;
     }
     
+    /**
+     * Post endpoint for /events.
+     * 
+     * Every event should be sent through
+     * this endpoint from slack. It will forward the event to any registered
+     * handler in the factory.
+     * 
+     * @param event the incoming event from slack
+     * @return the response from the handler or bad request if no handler is found.
+     */
     @POST
     public Response post(Event event) {
         Objects.requireNonNull(event);        
+
+        String type = event.getType();
+        if ("event_callback".equals(type)) {
+            type = event.getEvent().get("type").asText();
+        }
 
         return handlers.get(event.getType()).handle(event);
     }
