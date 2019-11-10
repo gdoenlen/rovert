18b57963ee8271bcc6bd2eb972c68a225d614ffe
George Doenlen <gdoenlen@gmail.com>
2019-11-10 18:22:56 -0500

    tests

---
+++ src/test/java/com/github/gdoenlen/rovert/EventHandlerFactoryTest.java
@@ -0,0 +1,29 @@
+package com.github.gdoenlen.rovert;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.github.gdoenlen.rovert.EventHandlerFactory.NoHandlerFoundException;
+
+import org.junit.jupiter.api.Test;
+
+class EventHandlerFactoryTest {
+    @Test
+    void it_should_return_the_handler_matching_the_type() {
+        var handler = new UrlVerificationHandler();
+        var factory = new EventHandlerFactory(handler);
+        assertTrue(handler == factory.get(handler.getEventType()));
+    }
+
+    @Test
+    void it_should_throw_NoHandlerFoundException_when_there_is_no_handler() {
+        var handler = new UrlVerificationHandler();
+        var factory = new EventHandlerFactory(handler);
+        try {
+            factory.get("hello");
+        } catch (NoHandlerFoundException ex) {
+            assertTrue(true);
+            return;
+        }
+        assertTrue(false);
+    }
+}
