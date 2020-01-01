e0bac60731fda95eff2af3f440ec3fba26694207
George Doenlen <gdoenlen@gmail.com>
2019-12-31 23:37:01 -0500

    add missing this

--- src/main/java/com/github/gdoenlen/rovert/EventHandlerFactory.java
+++ src/main/java/com/github/gdoenlen/rovert/EventHandlerFactory.java
@@ -7,11 +7,11 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 /**
- * Factory for getting the handlers for the slack events. 
+ * Factory for getting the handlers for the slack events.
  */
 @ApplicationScoped
 public class EventHandlerFactory {
-    
+
     private final Map<String, EventHandler> handlers = new HashMap<String, EventHandler>();
 
     @Inject
@@ -22,13 +22,13 @@ public class EventHandlerFactory {
 
     /**
      * Gets the event handler for the given type.
-     * 
+     *
      * @param type the type of the event from the slap api
      * @return the handler registered for the type
      * @throws NoHandlerFoundException when no handler is registered for that type
      */
     public EventHandler get(String type) {
-        EventHandler handler = handlers.get(type);
+        EventHandler handler = this.handlers.get(type);
         if (handler == null) {
             throw new NoHandlerFoundException("No event handler found for type: " + type);
         }
